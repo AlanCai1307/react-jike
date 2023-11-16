2153c183896d0570c86e5a12137b083c031f07a6
AlanCai <p_alanccai@tencent.com>
2023-11-16 16:04:26 +0800

    router配置

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import './index.scss';
-import App from './App';
+import router from "./router";
+import { RouterProvider } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} ></RouterProvider>
   </React.StrictMode>
 );
