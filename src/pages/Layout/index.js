3bc64939cddb5fb9bb713f4b0a1b239b24a270e8
AlanCai <p_alanccai@tencent.com>
2023-11-17 14:34:41 +0800

    二级路由配置

--- src/pages/Layout/index.js
+++ src/pages/Layout/index.js
@@ -6,6 +6,7 @@ import {
   LogoutOutlined,
 } from '@ant-design/icons'
 import './index.scss'
+import {Outlet} from "react-router-dom";
 
 const { Header, Sider } = Layout
 
@@ -51,7 +52,7 @@ const GeekLayout = () => {
             style={{ height: '100%', borderRight: 0 }}></Menu>
         </Sider>
         <Layout className="layout-content" style={{ padding: 20 }}>
-          内容
+          <Outlet/>
         </Layout>
       </Layout>
     </Layout>
