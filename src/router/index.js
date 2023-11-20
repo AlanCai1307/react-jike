cf647a4f999b345fdef938a6b799ffda6f634be4
AlanCai <p_alanccai@tencent.com>
2023-11-20 16:25:34 +0800

    优化-路由懒加载

--- src/router/index.js
+++ src/router/index.js
@@ -2,9 +2,10 @@ import Layout from '@/pages/Layout'
 import Login from '@/pages/Login'
 import {createBrowserRouter} from "react-router-dom";
 import {AuthRoute} from "@/components/AuthRoute";
-import Home from "@/pages/Home";
-import Article from "@/pages/Article";
-import Publish from "@/pages/Publish";
+import {lazy, Suspense} from "react";
+const Publish = lazy(() => import('@/pages/Publish'))
+const Article = lazy(() => import('@/pages/Article'))
+const Home = lazy(() => import('@/pages/Article'))
 const router = createBrowserRouter([
   {
     path: '/',
@@ -12,15 +13,27 @@ const router = createBrowserRouter([
     children: [
       {
         index: true,
-        element: <Home/>
+        element: (
+          <Suspense fallback={'加载中'}>
+            <Home />
+          </Suspense>
+        )
       },
       {
         path: 'article',
-        element: <Article/>
+        element: (
+          <Suspense fallback={'加载中'}>
+            <Article />
+          </Suspense>
+        )
       },
       {
         path: 'publish',
-        element: <Publish/>
+        element:(
+          <Suspense fallback={'加载中'}>
+            <Publish />
+          </Suspense>
+        )
       }
     ]
   },
